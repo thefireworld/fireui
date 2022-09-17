df4a8c7a98bf9fa293bd4fb20a560273ed424ad1
김시후 <me@iamsihu.wtf>
2022-09-17 11:45:47 +0900

    toss 파일 전송 방식 변경

--- lib/pages/firetoss.dart
+++ lib/pages/firetoss.dart
@@ -1,17 +1,65 @@
 import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
+import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
 import 'package:drag_and_drop_windows/drag_and_drop_windows.dart';
 import 'package:file_icon/file_icon.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:fire/main.dart';
 import 'package:fire/utils.dart';
 import 'package:floating_action_bubble/floating_action_bubble.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_platform_alert/flutter_platform_alert.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:hovering/hovering.dart';
+import 'package:internet_file/internet_file.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:qrscan/qrscan.dart';
 import 'package:sn_progress_dialog/progress_dialog.dart';
+
+void initializeFireToss() {
+  socket.on('tossed', (data) async {
+    Directory directory;
+    if (Platform.isAndroid || Platform.isIOS) {
+      directory = (await DownloadsPathProvider.downloadsDirectory)!;
+    } else {
+      directory = (await getDownloadsDirectory())!;
+    }
+    final clickedButton = await FlutterPlatformAlert.showCustomAlert(
+      windowTitle: '다른 기기에서 파일이 전송되었습니다.',
+      text: '파일 이름: (알수없음)',
+      positiveButtonTitle: "승인",
+      negativeButtonTitle: "거부",
+    );
+    if (clickedButton == CustomButton.positiveButton) {
+      for (var value in data) {
+        List<String> spl = value.split('/');
+        String name = spl[spl.length - 1];
+        name = name.substring(0, name.length - 13);
+        if (Platform.isAndroid || Platform.isIOS) {
+          await FlutterDownloader.enqueue(
+            url: "http://$value",
+            savedDir: directory.path,
+            fileName: name,
+            showNotification: true,
+            openFileFromNotification: true,
+          );
+        } else {
+          final Uint8List bytes = await InternetFile.get("http://$value",
+              progress: (receivedLength, contentLength) {});
+          File("${directory.path}/$name")
+            ..createSync(recursive: true)
+            ..writeAsBytesSync(decryptFile(bytes, "awesome password").toList());
+        }
+      }
+    }
+  });
+}
 
 class FireTossPage extends StatefulWidget {
   final List<String> defaultFiles;
@@ -58,6 +106,7 @@ class _FireTossPageState extends State<FireTossPage>
     if (Platform.isWindows) {
       subscription.cancel();
     }
+
     super.dispose();
   }
 
@@ -148,24 +197,65 @@ class _FireTossPageState extends State<FireTossPage>
               crossAxisSpacing: 0,
             ),
             itemBuilder: (BuildContext context, int index) {
-              return Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
+              Widget widget = Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FileIcon(
+                    files[index],
+                    size: 100,
+                  ),
+                  Text(
+                    File(files[index])
+                        .path
+                        .replaceAll(File(files[index]).parent.path, "")
+                        .substring(1),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              );
+              widget = HoverWidget(
+                hoverChild: Stack(
+                  alignment: AlignmentDirectional.center,
                   children: [
-                    FileIcon(
-                      files[index],
-                      size: 100,
+                    widget,
+                    Positioned(
+                      top: 30,
+                      right: 20,
+                      child: InkWell(
+                        onTap: () {
+                          setState(() {
+                            files.removeAt(index);
+                          });
+                        },
+                        child: const Icon(Icons.close),
+                      ),
                     ),
-                    Text(
-                        File(files[index])
-                            .path
-                            .replaceAll(File(files[index]).parent.path, "")
-                            .substring(1),
-                        overflow: TextOverflow.ellipsis),
                   ],
                 ),
+                onHover: (PointerEnterEvent event) {},
+                child: widget,
               );
+              if (index < 10 /*MAX FILES*/) {
+                return widget;
+              } else {
+                return Tooltip(
+                  message: "한번에 최대 10개의 파일을 보낼 수 있습니다.",
+                  child: Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: [
+                      widget,
+                      const Image(
+                        image: Svg(
+                          "assets/icons8-error.svg",
+                          color: Colors.red,
+                        ),
+                        width: 75,
+                      ),
+                    ],
+                  ),
+                );
+              }
             },
           ),
         ),
@@ -175,100 +265,97 @@ class _FireTossPageState extends State<FireTossPage>
 
   Future<void> sendFile(BuildContext context, String address) async {
     int startTime = DateTime.now().millisecondsSinceEpoch;
-    List<String> fileData = [];
-    for (int i = 1; i <= files.length; i++) {
-      File file = File(files[i - 1]);
-      var dio = Dio();
-      var formData = FormData.fromMap({
-        'file': MultipartFile.fromBytes(
-            encryptFile(file.readAsBytesSync(), "awesome password"),
-            filename: file.path.replaceFirst(file.parent.path, "").substring(1))
-      });
-      ProgressDialog pd = ProgressDialog(context: context);
-      pd.show(
-        max: 100,
-        msg: 'Sending files... ($i/${files.length})',
-      );
-      try {
-        final response = await dio.post(
-          'http://pc.iamsihu.wtf:3000/upload',
-          data: formData,
-          onSendProgress: (rec, total) {
-            pd.update(value: ((rec.toDouble() / total) * 100).toInt());
+    var dio = Dio();
+    List<MultipartFile> uploadFiles = [];
+    for (var path in files) {
+      File file = File(path);
+      uploadFiles.add(MultipartFile.fromBytes(
+          encryptFile(file.readAsBytesSync(), "awesome password"),
+          filename: file.path.replaceFirst(file.parent.path, "").substring(1)));
+    }
+
+    var formData = FormData.fromMap({'files': uploadFiles});
+    ProgressDialog pd = ProgressDialog(context: context);
+    pd.show(
+      max: 100,
+      msg: 'Sending files...',
+    );
+    try {
+      final response = await dio.post(
+        'http://pc.iamsihu.wtf:3000/upload',
+        data: formData,
+        onSendProgress: (rec, total) {},
+        options: Options(
+          headers: {
+            "authorization": "Basic 6BB6EEF72AD57F14F4B59F2C1AE2F",
           },
-          options: Options(
-            headers: {
-              "authorization": "Basic 6BB6EEF72AD57F14F4B59F2C1AE2F",
-            },
+        ),
+      );
+      if (response.statusCode == 401) {
+        await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text("ERROR"),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("OK"),
+              ),
+            ],
           ),
         );
-        if (response.statusCode == 401) {
-          await showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: const Text("ERROR"),
-              actions: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text("OK"),
+      }
+      socket.emit("toss", {
+        "files": response.data["files"],
+        "to": address,
+      });
+      int endTime = DateTime.now().millisecondsSinceEpoch;
+      DateTime time = DateTime.fromMillisecondsSinceEpoch(endTime - startTime);
+      log("걸린시간: ${time.hour - 9}시간 ${time.minute}분 ${time.second}초");
+    } catch (e) {
+      if (e is DioError) {
+        if (e.response!.data["status"] == "err") {
+          switch (e.response!.data["code"]) {
+            case "file_too_large":
+              await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text("잠시만요!"),
+                  content: const Text("파일이 너무 커요. 한 파일당 최대 용량은 1GB에요."),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("okay"),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          );
-        }
-        fileData.add(response.data["url"]);
-      } catch (e) {
-        if (e is DioError) {
-          if (e.response!.data["status"] == "err") {
-            switch (e.response!.data["code"]) {
-              case "file_too_large":
-                await showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text("잠시만요!"),
-                    content: const Text("파일이 너무 커요. 한 파일당 최대 용량은 1GB에요."),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text("okay"),
-                      ),
-                    ],
-                  ),
-                );
-                break;
-              default:
-                await showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text("잠시만요!"),
-                    content: Text(
-                        "오류가 발생했어요. (메시지: ${e.response!.data["message"]})"),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text("okay"),
-                      ),
-                    ],
-                  ),
-                );
-                break;
-            }
+              );
+              break;
+            default:
+              await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text("잠시만요!"),
+                  content:
+                      Text("오류가 발생했어요. (메시지: ${e.response!.data["message"]})"),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("okay"),
+                    ),
+                  ],
+                ),
+              );
+              break;
           }
         }
       }
     }
-    socket.emit("toss", {
-      "files": fileData,
-      "to": address,
-    });
-    int endTime = DateTime.now().millisecondsSinceEpoch;
-    DateTime time = DateTime.fromMillisecondsSinceEpoch(endTime - startTime);
-    log("걸린시간: ${time.hour - 9}시간 ${time.minute}분 ${time.second}초");
   }
 }
