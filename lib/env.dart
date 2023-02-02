4e23ec913b58fc894f1737c807d10f5ebe65c56e
김시후 <me@iamsihu.wtf>
2023-02-03 00:13:33 +0900

    시크릿

--- lib/env.dart
+++ lib/env.dart
@@ -5,5 +5,5 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'FIRE_API_KEY', obfuscate: true)
-  static final fireApiKey = "Shh! Can I tell you a SECRET?"
+  static final fireApiKey = "Shh! Can I tell you a SECRET?";
 }
